a57db5cbb98ad82d74ce3452b23ab26e7743b733
Impresys User <impresys@isc.impresys.com>
2020-02-05 13:53:17 -0800

    init, added bulk image edit script

---
+++ add_image_border.py
@@ -0,0 +1,47 @@
+import os, sys, glob
+from PIL import Image
+
+# Location of the image to be 'bordered' in the background tablet/phone shell
+# IMAGE_LOC[0] is horizontal x distance, IMAGE_LOC[1] is the vertical y distance,
+# with the uppermost leftmost point of the image as the origin
+IMAGE_LOC = (240, 96)
+IMAGE_RES = (1920, 1080)
+SCREEN_NEW_SIZE = (1438, 894) #size of screen after resize
+
+# File locations i
+ASSET_PATH = r"C:\Users\impresys\Documents\py_scripts\200205tablet\assets\d365_cfs_test.demo_Assets"
+BG_IMAGE_PATH = r"C:\Users\impresys\Documents\py_scripts\200205tablet\assets\tablet_bg.png"
+
+
+def main(take_path_input=False, in_demo_folder=False):
+    '''
+    Usage: In script folder, open terminal and run python main.py {demo asset folder}
+    '''
+    if take_path_input: 
+        os.chdir(sys.argv[1]) # --> python add_image_border.py {path\to\asset\folder}
+    elif in_demo_folder:
+        path = os.getcwd() # --> if place script in folder containing asset folder 
+    else:
+        path = ASSET_PATH
+
+    bg_img = Image.open(BG_IMAGE_PATH)
+    bg_img.show()
+
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.Png'):
+                new_path = os.path.join(subdir, file)
+                print(new_path)
+                
+                img = Image.open(new_path)
+                bg_copy = bg_img.copy()
+                img_resize = img.resize((SCREEN_NEW_SIZE), Image.ANTIALIAS)
+                bg_copy.paste(img_resize, IMAGE_LOC)
+                bg_copy.save(new_path)
+                print("Finished: " + new_path)
+                
+
+
+
+if __name__ == '__main__':
+    main()
