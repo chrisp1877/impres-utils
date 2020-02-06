204d762ddd7cef46a44702b8abbd9c4d3f7cd275
Impresys User <impresys@isc.impresys.com>
2020-02-06 15:06:14 -0800

    more tkinter

--- window_tk.py
+++ window_tk.py
@@ -2,27 +2,40 @@ from tkinter import *
 import add_image_border as paste
 
 def main():
-    window = Tk()
-    window.title("Impresys Bulk Image Edit Utility")
-    window.geometry('800x600')
+    root = Root()
+    root.mainloop()
 
-    greeting = Label(window, text= '''
+class Root(Tk):
+    def __init__(self):
+        super(Root, self).__init__()
+
+        self.bg_path
+        self.fg_path
+        self.img_loc
+        self.img_size
+
+        self.title("Impresys Bulk Image Edit Utility")
+        self.minsize(800, 600)
+
+        self.greeting = Label(self, text= '''
         Welcome to the Impresys bulk image pasting utility.
         To start, select background and foreground image:
-    ''')
-    greeting.grid(column=0, row=0)
+        ''')
+        self.greeting.grid(column=0, row=0, padx=20, pady=20)
 
-    ### ---
+        self.labelFrame = LabelFrame(self, text = "Open File")
+        self.labelFrame.grid(column = 0, row = 1, padx = 20, pady = 20)
+ 
+        self.open_bg_btn()
+        self.open_fg_btn()
+        
+        self.submit()
 
-    ### ---
+    def open_bg_btn(self):
+        pass
 
-    def submit():
-        submit_btn.configure(text="Submitting...")
-
-    submit_btn = Button(window, text="Click Me", command=submit)
-    submit_btn.grid(column=1, row=0)
-
-    window.mainloop()
+    def open_fg_btn(self):
+        pass
 
 if __name__ == '__main__':
     main()
