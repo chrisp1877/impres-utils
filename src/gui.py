6d66510509fff610a3050b92ce01c16e8370f9f6
chrisp1877 <chrisp1877@gmail.com>
2020-03-02 15:31:00 -0800

    gui additions

--- src/gui.py
+++ src/gui.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import (
     QApplication, QWidget, QMainWindow, QPushButton, QComboBox, QDialog,
     QDialogButtonBox, QFormLayout, QGridLayout, QGroupBox, QHBoxLayout,
     QLabel, QLineEdit, QMenu, QMenuBar, QPushButton, QSpinBox, QTextEdit,
-    QVBoxLayout, QFileDialog, QAction, QMessageBox
+    QVBoxLayout, QFileDialog, QAction, QMessageBox, QFrame
 )
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import pyqtSlot
@@ -22,31 +22,11 @@ class ImpresysApp(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
-        self.demo_dir_tbox = QLineEdit(self)
-        self.demo_dir_tbox.move(20, 40)
-        self.demo_dir_tbox.resize(200,30)
-
-        browse_demo = QPushButton('Browse...', self)
-        browse_demo.setToolTip('Browse for .demo file')
-        browse_demo.move(250,40)
-        browse_demo.clicked.connect(self.browse_demo)
-
-        self.bg_img_tbox = QLineEdit(self)
-        self.bg_img_tbox.move(20, 80)
-        self.bg_img_tbox.resize(200,30)
-
-        browse_bg = QPushButton('Browse...', self)
-        browse_bg.setToolTip('Browse for .demo file')
-        browse_bg.move(250,80)
-        browse_bg.clicked.connect(self.browse_img)
-
 
         mainMenu = self.menuBar()
         fileMenu = mainMenu.addMenu('File')
         editMenu = mainMenu.addMenu('Edit')
         viewMenu = mainMenu.addMenu('View')
-        searchMenu = mainMenu.addMenu('Search')
         toolsMenu = mainMenu.addMenu('Tools')
         helpMenu = mainMenu.addMenu('Help')
 
@@ -56,13 +36,63 @@ class ImpresysApp(QMainWindow):
         exitButton.triggered.connect(self.close)
         fileMenu.addAction(exitButton)
 
-        button = QPushButton('Shell/Insert', self)
-        button.setToolTip('Begin shelling/insertion')
-        button.move(600,500)
-        button.clicked.connect(self.on_click)
+        viewStatAct = QAction('View statusbar', self, checkable=True)
+        viewStatAct.setStatusTip('View statusbar')
+        viewStatAct.setChecked(True)
+        viewStatAct.triggered.connect(self.toggleMenu)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+        viewMenu.addAction(viewStatAct)
+        
+        self.createGridLayout()
+        windowLayout = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox)
+        self.setLayout(windowLayout)
         
         self.statusBar().showMessage('Ready for shelling/insertion')
         self.show()
+
+    def createGridLayout(self):
+        self.horizontalGroupBox = QGroupBox("Grid")
+        layout = QGridLayout()
+        layout.setSpacing(10)
+        layout.setColumnStretch(1,5)
+        layout.setColumnStretch(2,5)
+
+        ## ENTER RADIO BUTTON TO ASK FOR SHELLING OR INSERT
+        ## MAKE TEXT OF FOLLOWING TEXT BOXES CORRESPOND TO SEELECTION
+
+        ## ENTER RADIO BUTTON TO ASK IF USER WANTS TO ADD BG IMAGE AND SHELL IF SHELL
+        ## MAKE TEXT BELOW COHERE TO SELECTION
+
+
+        self.demo_dir_label = QLabel(self)
+        self.demo_dir_label.setText("Enter .demo file location:")
+        layout.addWidget(self.demo_dir_label, 0,1)
+        self.demo_dir_tbox = QLineEdit(self)
+        self.demo_dir_tbox.resize(200,30)
+        layout.addWidget(self.demo_dir_tbox, 1,1)
+        self.browse_demo_btn = QPushButton('Browse...', self)
+        self.browse_demo_btn.setToolTip('Browse for .demo file')
+        self.browse_demo_btn.clicked.connect(self.browse_demo)
+        layout.addWidget(self.browse_demo_btn, 2, 1)
+
+        self.bg_dir_label = QLabel(self)
+        self.bg_dir_label.setText("Enter bg image location:")
+        layout.addWidget(self.bg_dir_label, 0,2)
+        self.bg_img_tbox = QLineEdit(self)
+        self.bg_img_tbox.resize(200,30)
+        layout.addWidget(self.bg_img_tbox, 1,2)
+        self.browse_bg_btn = QPushButton('Browse...', self)
+        self.browse_bg_btn.setToolTip('Browse for .demo file')
+        self.browse_bg_btn.clicked.connect(self.browse_img)
+        layout.addWidget(self.browse_bg_btn, 2,2)
+
+        self.submit = QPushButton('Shell/Insert', self)
+        self.submit.setToolTip('Begin shelling/insertion')
+        self.submit.clicked.connect(self.on_click)
+        layout.addWidget(self.submit, 3, 4)
+
+        self.horizontalGroupBox.setLayout(layout)
+
 
     def browse_demo(self):
         options = QFileDialog.Options()
@@ -79,6 +109,10 @@ class ImpresysApp(QMainWindow):
         if fileName:
             print(fileName)
         self.demo_dir_tbox.text = fileName
+
+    def toggleMenu(self, state):
+        if state: self.statusBar.show()
+        else: self.statusBar.hide()
     
     @pyqtSlot()
     def on_click(self):
